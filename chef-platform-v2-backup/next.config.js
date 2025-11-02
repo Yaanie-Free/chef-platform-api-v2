fefcf3ed64c89f864681d3eef8bb3c13bd06ad76
Daiyaan <daiyaanf@tfg.co.za>
2025-11-02 19:07:14 +0200

    commit

--- chef-platform-v2-backup/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // This is a minimal configuration.
-  // We can add the asset prefix or analyzer configuration later if needed,
-  // but keeping it simple is best for solving the error now.
-};
-
-module.exports = nextConfig;
